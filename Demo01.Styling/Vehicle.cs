07174a0330c03dcaa173b6bc0257c1d101c6f006
Christopher Brzostowski <kbrzostowski@gmail.com>
2019-05-29 21:32:07 +0200

    Style x:key

---
+++ Demo01.Styling/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01.Styling
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string Color { get; set; }
+        public string Branch { get; set; }
+    }
+}
